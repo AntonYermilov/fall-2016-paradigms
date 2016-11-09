0a2feb45accf391c87856084b12510df2c93e03c
AntonYermilov <anton.yermilov@gmail.com>
2016-11-09 22:15:30 +0300

    trash removed

--- clean.sh
+++
@@ -1,7 +0,0 @@
-find * -type d | while read d; do
-	#value = echo `expr match "$d" ".*"`
-	if [[ $d =~ .*__pycache__ ]]; then
-		echo $d
-		rm -rf $d
-	fi
-done
--- task160914/gen_test.py
+++
@@ -1,5 +0,0 @@
-import random
-
-with open('test', 'w') as f:
-    for i in range(10000000):
-        f.write(str(random.randint(0, 9)))
