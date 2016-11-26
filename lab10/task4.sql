d93f5632c10725b0232bd400d3dd91d197bf57f7
AntonYermilov <anton.yermilov@gmail.com>
2016-11-26 21:13:59 +0300

    tasks 4,8 fixed

--- lab10/task4.sql
+++ lab10/task4.sql
@@ -1,6 +1,6 @@
 --4--
-select Country.Name, sum(Country.Code = City.CountryCode and City.Population >= 1000000)
+select Country.Name, sum(Country.Code = City.CountryCode and City.Population >= 1000000) CNT
     from Country, City
     group by Country.Name
-    order by sum(Country.Code = City.CountryCode and City.Population >= 1000000) desc, Country.Name;
+    order by CNT desc, Country.Name;
 
--- lab10/task8.sql
+++ lab10/task8.sql
@@ -1,10 +1,10 @@
 --8--
 select Country.Name, Country.Population, Country.SurfaceArea
     from Country
-    inner join City on Country.Code = City.CountryCode
+    inner join City JustCity on Country.Code = JustCity.CountryCode
     inner join Capital on Country.Code = Capital.CountryCode
-    where City.Id = Capital.CityId and City.Population < (select City.Population
-                                                              from City
-                                                              where City.CountryCode = Country.Code)
+    inner join City CapitalCity on Capital.CityId = CapitalCity.Id
+    group by Country.Name
+    having max(JustCity.Population) and JustCity.Id <> CapitalCity.Id
     order by (1.0 * Country.Population / Country.SurfaceArea) desc, Country.Name;
 
